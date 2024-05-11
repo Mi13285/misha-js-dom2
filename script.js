bf7f732440fcdc0bb616391ac0d421edcf112d08
Mi13285 <mixkud95@inbox.ru>
2024-05-11 16:23:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let Element = document.body.firstElementChild;
+console.log(Element);
+let ulElement = document.body.children[1];
+console.log(ulElement);
+let element = ulElement.children[1];
+console.log(element);
